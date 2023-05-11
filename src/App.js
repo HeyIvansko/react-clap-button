666e8ef5972d81c0cf2a95e7b7e2204c315466b1
Ivansko <ivanxsoumah@gmail.com>
2023-05-11 16:32:57 +0000

    code source

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHandsClapping } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClap = () => {
+    if (count < 20) {
+      setCount(count + 1);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card-body">
+        <h2>Clap Button</h2>
+        <div className="counter">{count}</div>
+        <button className="btn btn-success mx-3 btn-large" onClick={handleClap}>
+          <FontAwesomeIcon icon={faHandsClapping} />
+        </button>
+        
+      </div>
     </div>
   );
 }
 
 export default App;
+
